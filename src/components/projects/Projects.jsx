d6ae4971a020d7ced2c14fa3af14513da7832824
Mustafa <mostafakhaled0787314@gmail.com>
2023-06-23 20:00:47 +0300

    last

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,8 @@ import styles from "./projects.module.css";
 const Projects = () => {
   return (
     <div className={styles.projects} id="projects">
+      <h2 className={styles.head}>Projects</h2>
+
       <div className={`${styles.projects_content} second-container`}>
         <Project />
       </div>
